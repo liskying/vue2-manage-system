91e36654a24cca83d312b3942f9ded09cf314bcc
liskying <liwenxue1012@163.com>
2017-10-06 21:17:35 +0800

    自定义组件并实现路由访问

---
+++ src/components/Test.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="test">
+    {{Message}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'test',
+  data () {
+    return {
+      Message: '自定义VUe组件测试'
+    }
+  }
+}
+</script>
